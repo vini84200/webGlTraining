863b04239cc77f3de42742e3ae37f136a97175ba
vini84200 <vini84200@debian>
2020-11-12 12:09:01 -0300

    Usando crases

--- out/hello-triangle.js
+++ out/hello-triangle.js
@@ -1,25 +1,23 @@
-var vertexShaderText = [
-    'precision mediump float;',
-    '',
-    'attribute vec2 vertPosition;',
-    'attribute vec3 vertColor;',
-    'varying vec3 fragColor;',
-    '',
-    'void main()',
-    '{',
-    '   fragColor = vertColor;',
-    '   gl_Position = vec4(vertPosition, 0.0, 1.0);',
-    '}'
-].join('\n');
-var fragmentShaderText = [
-    'precision mediump float;',
-    '',
-    'varying vec3 fragColor;',
-    'void main()',
-    '{',
-    'gl_FragColor = vec4(fragColor, 1.0);',
-    '}'
-].join('\n');
+var vertexShaderText = `precision mediump float;
+    
+    attribute vec2 vertPosition;
+    attribute vec3 vertColor;
+    varying vec3 fragColor;
+    
+    void main()
+    {
+       fragColor = vertColor;
+       gl_Position = vec4(vertPosition, 0.0, 1.0);
+    }
+`;
+var fragmentShaderText = `precision mediump float;
+    
+    varying vec3 fragColor;
+    void main()
+    {
+        gl_FragColor = vec4(fragColor, 1.0);
+    }
+`;
 var InitDemo = function () {
     var canvas = document.getElementById('game-surface');
     var gl = canvas.getContext('webgl');
--- src/hello-triangle.ts
+++ src/hello-triangle.ts
@@ -1,26 +1,25 @@
-var vertexShaderText = [
-    'precision mediump float;',
-    '',
-    'attribute vec2 vertPosition;',
-    'attribute vec3 vertColor;',
-    'varying vec3 fragColor;',
-    '',
-    'void main()',
-    '{',
-    '   fragColor = vertColor;',
-    '   gl_Position = vec4(vertPosition, 0.0, 1.0);',
-    '}'
-].join('\n')
-
-var fragmentShaderText = [
-    'precision mediump float;',
-    '',
-    'varying vec3 fragColor;',
-    'void main()',
-    '{',
-    'gl_FragColor = vec4(fragColor, 1.0);',
-    '}'
-].join('\n')
+var vertexShaderText = 
+    `precision mediump float;
+    
+    attribute vec2 vertPosition;
+    attribute vec3 vertColor;
+    varying vec3 fragColor;
+    
+    void main()
+    {
+       fragColor = vertColor;
+       gl_Position = vec4(vertPosition, 0.0, 1.0);
+    }
+`
+var fragmentShaderText =
+    `precision mediump float;
+    
+    varying vec3 fragColor;
+    void main()
+    {
+        gl_FragColor = vec4(fragColor, 1.0);
+    }
+`
 
 var InitDemo = function () {
     var canvas = <HTMLCanvasElement> document.getElementById('game-surface')

4f453b1a540111f244928c50f0948c1d35e27f5e
vini84200 <vini84200@debian>
2020-11-13 11:34:00 -0300

    Rotating cube

--- out/src/rotating-cube.js
+++ out/src/rotating-cube.js
@@ -33,6 +33,10 @@ export const InitDemo = function () {
     // Resetando o fundo
     gl.clearColor(0.75, 0.85, 0.8, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    gl.frontFace(gl.CCW);
+    gl.cullFace(gl.BACK);
     // Shaders
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -65,15 +69,64 @@ export const InitDemo = function () {
     //
     // Create Buffer
     //
-    var triangleVertices = [
-        // X,   Y,   Z,         R,   G,   B 
-        0.0, 0.5, 0.0, 1.0, 1.0, 0.0,
-        -0.5, -0.5, 0.0, 1.0, 0.0, 1.0,
-        0.5, -0.5, 0.0, 0.1, 1.0, 0.6,
+    var boxVertices = [
+        // Top
+        -1.0, 1.0, -1.0, 0.5, 0.5, 0.5,
+        -1.0, 1.0, 1.0, 0.5, 0.5, 0.5,
+        1.0, 1.0, 1.0, 0.5, 0.5, 0.5,
+        1.0, 1.0, -1.0, 0.5, 0.5, 0.5,
+        // Left
+        -1.0, 1.0, 1.0, 0.75, 0.25, 0.5,
+        -1.0, -1.0, 1.0, 0.75, 0.25, 0.5,
+        -1.0, -1.0, -1.0, 0.75, 0.25, 0.5,
+        -1.0, 1.0, -1.0, 0.75, 0.25, 0.5,
+        // Right
+        1.0, 1.0, 1.0, 0.25, 0.25, 0.75,
+        1.0, -1.0, 1.0, 0.25, 0.25, 0.75,
+        1.0, -1.0, -1.0, 0.25, 0.25, 0.75,
+        1.0, 1.0, -1.0, 0.25, 0.25, 0.75,
+        // Front
+        1.0, 1.0, 1.0, 1.0, 0.0, 0.15,
+        1.0, -1.0, 1.0, 1.0, 0.0, 0.15,
+        -1.0, -1.0, 1.0, 1.0, 0.0, 0.15,
+        -1.0, 1.0, 1.0, 1.0, 0.0, 0.15,
+        // Back
+        1.0, 1.0, -1.0, 0.0, 1.0, 0.15,
+        1.0, -1.0, -1.0, 0.0, 1.0, 0.15,
+        -1.0, -1.0, -1.0, 0.0, 1.0, 0.15,
+        -1.0, 1.0, -1.0, 0.0, 1.0, 0.15,
+        // Bottom
+        -1.0, -1.0, -1.0, 0.5, 0.5, 1.0,
+        -1.0, -1.0, 1.0, 0.5, 0.5, 1.0,
+        1.0, -1.0, 1.0, 0.5, 0.5, 1.0,
+        1.0, -1.0, -1.0, 0.5, 0.5, 1.0,
     ];
-    var triangleVertexBufferObject = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+    var boxIndices = [
+        // Top
+        0, 1, 2,
+        0, 2, 3,
+        // Left
+        5, 4, 6,
+        6, 4, 7,
+        // Right
+        8, 9, 10,
+        8, 10, 11,
+        // Front
+        13, 12, 14,
+        15, 14, 12,
+        // Back
+        16, 17, 18,
+        16, 18, 19,
+        // Bottom
+        21, 20, 22,
+        22, 20, 23
+    ];
+    var boxVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
+    var boxIndexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
     var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
     var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
     gl.vertexAttribPointer(positionAttribLocation, // att location
@@ -100,7 +153,7 @@ export const InitDemo = function () {
     var viewMatrix = new Float32Array(16);
     var projMatrix = new Float32Array(16);
     mat4.identity(worldMatrix);
-    mat4.lookAt(viewMatrix, [0, 0, -2], [0, 0, 0], [0, 1, 0]);
+    mat4.lookAt(viewMatrix, [0, 2, -6], [0, 0, 0], [0, 1, 0]);
     mat4.perspective(projMatrix, toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
     gl.uniformMatrix4fv(matWorldUniformLocation, false, worldMatrix);
     gl.uniformMatrix4fv(matViewUniformLocation, false, viewMatrix);
@@ -117,7 +170,7 @@ export const InitDemo = function () {
         gl.uniformMatrix4fv(matWorldUniformLocation, false, worldMatrix);
         gl.clearColor(0.75, 0.85, 0.8, 1.0);
         gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-        gl.drawArrays(gl.TRIANGLES, 0, 3);
+        gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
         requestAnimationFrame(loop);
     };
     requestAnimationFrame(loop);
